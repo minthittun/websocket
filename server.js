90443d66a4717a06ef2a3c23d12a78a0a4df1768
Min Thit Tun <minthittun@gmail.com>
2018-12-03 17:06:33 +0630

    changed ws code

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 'use strict';
 var express = require('express')
-var ws = require('./ws')
-var port = process.env.PORT || 1337;
+const SocketServer = require('ws').Server;
+var PORT = process.env.PORT || 1337;
 var app = express()
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        // Broadcast to everyone else.
+        console.log("Message", data)
+        wss.clients.forEach(function each(client) {
+            //if (client !== ws && client.readyState === WebSocket.OPEN) {
+            //    client.send(data);
+            //}
+            client.send(data);
+        });
+    });
+});
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!')
+app.listen(PORT, function () {
+    console.log('Example app listening on port ' + PORT + '!')
 })
